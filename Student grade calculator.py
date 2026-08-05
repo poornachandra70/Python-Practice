58e3ae3348d3ad4bcfd3ad76d6003ba3e05808c3
poornachandra <deepakjabin407@gmail.com>
2026-08-05 23:42:38 +0530

    Fix Student grade calculator indentation and logic

---
+++ Student grade calculator.py
@@ -0,0 +1,15 @@
+marks = int(input())
+# Check whether the marks are valid
+if marks < 0 or marks > 100:
+    print("Invalid Marks")
+elif marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
