b7c4fcf8115508358558573b6f09002c8bdac792
poornachandra <deepakjabin407@gmail.com>
2026-08-06 22:10:13 +0530

    update coount numbers div by three

--- claculate the sum of digits.py
+++ claculate the sum of digits.py
@@ -3,6 +3,6 @@ sum_of_digits = 0
 while(number > 0):
 dig = number % 10
 sum_of_digits += dig
-number = number//10
+number = number
 # Extract and add each digit using a whi
 print(f"Sum of Digits: {sum_of_digits}")
