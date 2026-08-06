fd15aa6c3e107052ef0f0b7a5528c0e993ed9c07
poornachandra <deepakjabin407@gmail.com>
2026-08-06 22:14:21 +0530

    update count vowels in string using the direct iterstion

---
+++ count vowels in string using the direct iteration.py
@@ -0,0 +1,8 @@
+text = input()
+
+vowel_count = 0
+# iterate directly over the string and count the vowels 
+for char in text:
+    if char in "aeiou"
+    vowel_count = vowel_count + 1
+print(f"Vowel Count : {vowel_count}")
