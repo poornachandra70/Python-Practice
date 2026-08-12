e37b8a6b8314bbe837c119a3c6126d1f08e32f0f
poornachandra <deepakjabin407@gmail.com>
2026-08-12 14:45:35 +0530

    update code

---
+++ Calculate Weekly Study Hours Using a Function.py
@@ -0,0 +1,10 @@
+def display_study_hours():
+    # Read the inputs, calculate the total and print it
+    hours_per_day = int(input())
+    study_days = int(input())
+    
+    total_hours = hours_per_day * study_days
+    print(f"Total Study Hours: {total_hours}")
+
+# Call the function
+display_study_hours()
